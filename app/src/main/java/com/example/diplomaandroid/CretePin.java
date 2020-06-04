1d3fb5a399f9dd936c1b9d5789243d5f2d410c97
RodionKunafin <george.t.o.p@mail.ru>
2020-06-04 12:01:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diplomaandroid/CretePin.java
@@ -0,0 +1,50 @@
+package com.example.diplomaandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CretePin extends AppCompatActivity {
+    //final String SAVED_NEW_PIN = "saved_new_pin";
+    final String SAVED_PIN = "saved_pin";
+    EditText inputNewPin;
+    Button btnSaveNewPin;
+    Button btnReturnToPin;
+    SharedPreferences sharedPrefCreatePin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_pin);
+
+        inputNewPin = findViewById(R.id.inputNewPin);
+
+        btnSaveNewPin = findViewById(R.id.btnSaveNewPin);
+        btnSaveNewPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPrefCreatePin = getSharedPreferences("PinPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPrefCreatePin.edit();
+                editor.putString(SAVED_PIN, inputNewPin.getText().toString());
+                editor.apply();
+                Toast.makeText(CretePin.this, "PIN saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnReturnToPin = findViewById(R.id.btnReturnToPin);
+        btnReturnToPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CretePin.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/diplomaandroid/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.diplomaandroid;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final String SAVED_PIN = "saved_pin";
+    EditText inputPin;
+    Button btnSavePin;
+    Button btnSign;
+    Button btnCreatePin;
+    //public static final String APP_PREFERENCES_PIN = "pin";
+    SharedPreferences sPref;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputPin = findViewById(R.id.inputPin);
+        btnSavePin = findViewById(R.id.btnSavePin);
+        btnSavePin.setOnClickListener(this);
+        btnSign = findViewById(R.id.btnSign);
+        btnSign.setOnClickListener(this);
+
+        btnCreatePin = findViewById(R.id.btnCreatePin);
+        btnCreatePin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CretePin.class);
+                startActivity(intent);
+            }
+        });
+        SignPin();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSavePin:
+                savePin();
+                break;
+            case R.id.btnSign:
+                SignPin();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void savePin() {
+        sPref = getSharedPreferences("PinPref", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(SAVED_PIN, inputPin.getText().toString());
+        ed.apply();
+        Toast.makeText(MainActivity.this, "PIN saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void SignPin() {
+        sPref = getSharedPreferences("PinPref", MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_PIN, "");
+        inputPin.setText(savedText);
+        Toast.makeText(MainActivity.this, "Text loaded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SignPin();
+    }
+}
